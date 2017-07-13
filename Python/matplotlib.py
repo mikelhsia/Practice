3afdddfc09e643b6468a73261cfb6ba88af668f4
mikelhsia <mikelhsia@hotmail.com>
2017-07-13 18:55:05 +0800

    numpy module

--- Python/matplotlib.py
+++ Python/matplotlib.py
@@ -1,10 +1,16 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 
-# x = np.linspace(1, 2*np.pi, 50)
+x = np.linspace(1, 2*np.pi, 50)
 # plt.plot(x, np.sin(x), x, np.cos(x), x, np.tan(x))
 # plt.show()
 
-x = np.random.randn(1000)
-plt.hist(x, 50)
-plt.show()
+# x = np.random.randn(1000)
+# plt.hist(x, 50)
+# plt.show()
+
+a = np.arange(5)
+print a.dtype
+print a
+print a.shape 
+
