7168eec6907f0d48019f540f2602d0c375330059
mikelhsia <mikelhsia@hotmail.com>
2017-07-27 12:51:58 +0800

    Use Next page link to run through all pages. Change the Depth from 1 to 20 in setting.py

--- Python/scrapyItemLoader/scrapyItemLoader/spiders/sil_spider.py
+++ Python/scrapyItemLoader/scrapyItemLoader/spiders/sil_spider.py
@@ -22,32 +22,25 @@ class SilSpiderSpider(scrapy.Spider):
 		# print "http://manhua.fzdm.com/%d/%d/" % (manga, targetChap+x)
 
 	def parse(self, response):
-		urlList = response.xpath('//div[@class="navigation"]/a/@href').extract()
-		# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-		# next_list = response.xpath('//a[@id="mhona"]/@href').extract()
-		# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-		# To pop the last url item, which is duplicate with the index_1.html
-		urlList.pop()
-		print urlList
+		# meta字段是传递值的方法。在调试时返回的response中会出现meta的内容，它是一个字典，
+		# 故在传递时可以直接通过 response.meta['front_image_url']进行引用
+		# (也可以使用get的方法，附默认值防止出现异常）
+		# 加了dont_filter=True的参数就完全可以用了！Why!?
+		# self.log("1. [URL Parsing and Downloading Image]: %s" % response.url)
+		yield Request(url=response.url, callback=self.parse_detail, dont_filter=True)
 
-		for url in urlList:
-			# 加了dont_filter=True的参数就完全可以用了！Why!?
-			# meta字段是传递值的方法。在调试时返回的response中会出现meta的内容，它是一个字典，
-			# 故在传递时可以直接通过 response.meta['front_image_url']进行引用
-			# (也可以使用get的方法，附默认值防止出现异常）
-			yield Request(url=urlparse.urljoin(response.url, url), callback=self.parse_detail, dont_filter=True)
-			# self.log("[URL List Yield]: %s" % urlparse.urljoin(response.url, url))
+		try:
+			next_link = response.xpath(u'//a[contains(text(),"下一页")]/@href').extract().pop()
+			# self.log("2. [URL Parsing - Next link]: %s" % next_link)
 
-		'''
-		5.递归爬取网页
-		上述代码仅仅实现了一个url的爬取，如果该url的爬取的内容中包含了其他url，而我们也想对其进行爬取，那么如何实现递归爬取网页呢？
+			if next_link:
+				# self.log("3. [URL Parsing - Goes to next link]: %s" % next_link)
+				yield Request(url=urlparse.urljoin(response.url, next_link), callback=self.parse, dont_filter=True)
+				# 注：可以修改settings.py 中的配置文件，以此来指定“递归”的层数，如： DEPTH_LIMIT = 1
+		except:
+			# self.log("4. [URL Parsing]: Nothing to be poped")
+			pass
 
-		示例代码：
-		获取所有的url，继续访问，并在其中寻找相同的url
-		即通过yield生成器向每一个url发送request请求，并执行返回函数parse，
-		从而递归获取校花图片和校花姓名学校等信息。
-		注：可以修改settings.py 中的配置文件，以此来指定“递归”的层数，如： DEPTH_LIMIT = 1
-		'''
 
 	def parse_detail(self, response):
 		# print "[Response URL]: " , response.url
