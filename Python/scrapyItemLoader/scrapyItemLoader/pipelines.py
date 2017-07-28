e1f3c606a9e9ca4fbe81ed6f548dc834b724cff5
mikelhsia <mikelhsia@hotmail.com>
2017-07-28 17:24:34 +0800

    1. Enable pipeline py script\n2. Options to process item, drop item, or import item to json file\n3. open_spider and close_spider that execute before and after spider runs

--- Python/scrapyItemLoader/scrapyItemLoader/pipelines.py
+++ Python/scrapyItemLoader/scrapyItemLoader/pipelines.py
@@ -9,6 +9,10 @@ import os
 import urllib
 
 class ScrapyitemloaderPipeline(object):
+
+	def open_spider(self, spider):
+		print """######################\n# Now we're about to awake the spider\n######################"""
+
 	def process_item(self, item, spider):
 
 		# Creating all the folder and file necessary
@@ -22,3 +26,17 @@ class ScrapyitemloaderPipeline(object):
 
 		urllib.urlretrieve(item['imgSrc'], dst)
 		return item
+
+		# Option 2: you can Drop the item if you think the data in this item is incorrect
+		##########################################################################
+		# from scrapy.exceptions import DropItem
+		# raise DropItem("Missing price in %s" % item)
+
+		# Option 3: you can dump the item to json file
+		##############################################
+		# import json
+		# line = json.dumps(dict(item)) + "\n"
+		# self.file.write(line)
+
+	def close_spider(self, spider):
+		print """######################\n# Now we're about to put the spider to sleep\n ######################"""
